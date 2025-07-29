28349efafb64aa7fde55e05fc4df8fad1c6ab4d0
Rulft44 <dimucciorubengermania@gmail.com>
2025-07-29 17:35:53 +0100

    bruh

--- src/main/java/io/github/rulft44/gunner/Gunner.java
+++ src/main/java/io/github/rulft44/gunner/Gunner.java
@@ -1,15 +1,15 @@
-package io.github.username.modid;
+package io.github.rulft44.gunner;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ModId implements ModInitializer {
-	public static final String ID = "mod_id";
+public class Gunner implements ModInitializer {
+	public static final String ID = "gunner";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
 
 	@Override
 	public void onInitialize() {
-		//LOGGER.info("[Mod ID] pretty pink princess ponies prancing perpendicular");
+		LOGGER.info("[Gunner] skibidi dop dop dop yes yes yes (ts so buns :wilted_rose:)");
 	}
 }
