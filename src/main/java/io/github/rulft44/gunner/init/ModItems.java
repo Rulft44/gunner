32dae3441a402b2ab99f739eb0b6e3c3706b3a38
Rulft44 <dimucciorubengermania@gmail.com>
2025-08-13 11:00:35 +0100

    :sparkles: Creative Mode Tab

--- src/main/java/io/github/rulft44/gunner/init/ModItems.java
+++ src/main/java/io/github/rulft44/gunner/init/ModItems.java
@@ -2,13 +2,17 @@ package io.github.rulft44.gunner.init;
 
 import io.github.rulft44.gunner.Gunner;
 import io.github.rulft44.gunner.item.ConfettiGunItem;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import java.util.function.Function;
@@ -30,10 +34,26 @@ public class ModItems {
 	public static final Item CONFETTI_GUN = register("confetti_gun", ConfettiGunItem::new, new Item.Settings().maxCount(1).enchantable(2).useCooldown(0.5f));
 	public static final Item GUN_COMPONENT = register("gun_component", Item::new, new Item.Settings().maxCount(16));
 
+	public static final RegistryKey<ItemGroup> CUSTOM_ITEM_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(Gunner.ID, "item_group"));
+	public static final ItemGroup CUSTOM_ITEM_GROUP = FabricItemGroup.builder()
+		.icon(() -> new ItemStack(ModItems.CONFETTI_GUN))
+		.displayName(Text.translatable("itemGroup.gunner"))
+		//.texture(Identifier.of(Gunner.ID, "textures/item/gun_component"))
+		.build();
+
 	public static void initialize() {
 			ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS)
 			.register((itemGroup) -> itemGroup.add(ModItems.CONFETTI_GUN));
 			ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
 			.register((itemGroup) -> itemGroup.add(ModItems.GUN_COMPONENT));
+
+		// Register the group.
+		Registry.register(Registries.ITEM_GROUP, CUSTOM_ITEM_GROUP_KEY, CUSTOM_ITEM_GROUP);
+
+		// Register items to the custom item group.
+		ItemGroupEvents.modifyEntriesEvent(CUSTOM_ITEM_GROUP_KEY).register(itemGroup -> {
+			itemGroup.add(ModItems.CONFETTI_GUN);
+			itemGroup.add(ModItems.GUN_COMPONENT);
+		});
 	}
 }
